d7c042d940cbcc2472f06e973f6b4e5c16034c2d
marcooceanblue <marcochow@oceanbluescm.com>
2024-05-12 23:28:23 +0800

    refine sh

--- clear-docker-resources.sh
+++ clear-docker-resources.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Stop all running containers
-echo "Stopping all Docker containers..."
+echo "----- Stopping all Docker containers..."
 docker stop $(docker ps -aq)
 
 # Remove all containers
-echo "Removing all Docker containers..."
+echo "----- Removing all Docker containers..."
 docker rm $(docker ps -aq)
 
 # Remove all images
@@ -13,11 +13,11 @@ docker rm $(docker ps -aq)
 #docker rmi -f $(docker images -q)
 
 # Remove all volumes
-echo "Removing all Docker volumes..."
+echo "----- Removing all Docker volumes..."
 docker volume rm $(docker volume ls -q)
 
 # Remove all networks (skip default networks)
-echo "Removing all Docker networks..."
+echo "----- Removing all Docker networks..."
 docker network ls | grep "bridge\|none\|host" -v | awk '{if(NR>1) print $1}' | xargs docker network rm
 
-echo "Docker cleanup complete."
+echo "----- Docker cleanup complete."
